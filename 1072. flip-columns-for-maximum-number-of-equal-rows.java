9288e94ee10ba17bc50a2631010277d3aff6034d
Aditya Nandan <cse.adityanandan@gmail.com>
2024-11-29 23:38:26 +0530

    22/11/2024

---
+++ 1072. flip-columns-for-maximum-number-of-equal-rows.java
@@ -0,0 +1,29 @@
+# 22/11/2024
+# Question no. 1072 (https://leetcode.com/problems/flip-columns-for-maximum-number-of-equal-rows/description/)
+# Solution in Java
+
+class Solution {
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+        int n = matrix[0].length;
+        int maxRows = 0;
+
+        for (int[] currRow : matrix) {
+            int[] inverted = new int[n];
+            int count = 0;
+
+            for (int col = 0; col < n; col++) {
+                inverted[col] = currRow[col] == 0 ? 1 : 0;
+            }
+
+            for (int[] row : matrix) {
+                if (Arrays.equals(row, currRow) || Arrays.equals(row, inverted)) {
+                    count++;
+                }
+            }
+            maxRows = Math.max(maxRows, count);
+        }
+        return maxRows;
+    }
+}
+
+
