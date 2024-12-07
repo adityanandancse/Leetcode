deb89b91a71484cf0cef96c34b50923858d6ca4a
Aditya Nandan <cse.adityanandan@gmail.com>
2024-12-07 21:57:47 +0530

    7/12/2024

---
+++ 1760. Minimum Limit of Balls in a Bag.cpp
@@ -0,0 +1,35 @@
+// 7/12/2024
+//   (https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/description/)
+
+  
+  class Solution {
+    bool canAssign(vector<int>& nums,int val,int operations){
+        int count = 0;
+        for(int& ele: nums){
+            if(ele > val)
+            count += ceil((double)(ele - val)/val);
+        }
+        return count<=operations;
+    }
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int n = nums.size();
+
+        int low = 1;
+        int high = *max_element(nums.begin(),nums.end());
+        int mid;
+        int res = INT_MAX;
+
+        while(low<=high){
+            mid = low+(high-low)/2;
+
+            if(canAssign(nums,mid,maxOperations)){
+                res = mid;
+                high=mid-1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
+};
